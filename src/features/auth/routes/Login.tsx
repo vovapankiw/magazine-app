13043fd0daed08b86381b4ac3a6872eb293643b9
Volodymyr Pankiv <vpanki@softserveinc.com>
2023-08-23 13:55:00 +0300

    changed login btn

--- src/features/auth/routes/Login.tsx
+++ src/features/auth/routes/Login.tsx
@@ -1,12 +1,21 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Box, Button } from '@mui/material';
+import { Box } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Fingerprint from '@mui/icons-material/Fingerprint';
 
 export const Login = () => {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <Box>
-      <Button onClick={() => loginWithRedirect()}>Login</Button>
+    <Box height="100vh" width="100%" display="flex" justifyContent="center" alignItems="center">
+      <IconButton
+        aria-label="fingerprint"
+        color="success"
+        size="large"
+        onClick={() => loginWithRedirect()}
+      >
+        <Fingerprint />
+      </IconButton>
     </Box>
   );
 };
