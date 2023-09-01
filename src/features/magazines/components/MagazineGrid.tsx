a69501557f504cda8c49d1b3614c93fc65101e3c
vovapankiw <vovapankiw@gmail.com>
2023-09-01 13:17:02 +0300

    transform jpg to webp

--- src/features/magazines/components/MagazineGrid.tsx
+++ src/features/magazines/components/MagazineGrid.tsx
@@ -1,13 +1,31 @@
 /* eslint-disable */
 
-import { Box } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 import { InfiniteData } from '@tanstack/react-query';
+import Image from '../../../assets/images/cosmopolitan.webp';
 
 export const MagazineGrid = ({ data }: { data: InfiniteData<unknown> | undefined }) => {
-  console.log(data);
   return (
     <div>
-      {data?.pages?.map((page, i) => {
+      <Card sx={{ maxWidth: 205 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="245"
+            image={require('../../../assets/images/cosmopolitan.webp')}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="body2" component="div">
+              Lizard
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      {/* <img height="245px" src={require('../../../assets/images/cosmopolitan.webp')} />
+      <img height="245px" src={require('../../../assets/images/aaa_living.webp')} />
+      <img height="245px" src={require('../../../assets/images/national_geographic.webp')} /> */}
+      {/* {data?.pages?.map((page, i) => {
         return (
           <div key={i}>
             {(page as unknown as any).map((el: any) => {
@@ -15,7 +33,7 @@ export const MagazineGrid = ({ data }: { data: InfiniteData<unknown> | undefined
             })}
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
