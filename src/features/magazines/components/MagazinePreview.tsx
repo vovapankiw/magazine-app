b371502ae644a54d293709614dc971b81fdb5fc7
vovapankiw <vovapankiw@gmail.com>
2023-09-10 14:07:30 +0300

    added snack in preview component

--- src/features/magazines/components/MagazinePreview.tsx
+++ src/features/magazines/components/MagazinePreview.tsx
@@ -1,3 +1,4 @@
+import { useSnackbar } from 'notistack';
 import { Chip, Grid } from '@mui/material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
@@ -7,48 +8,61 @@ type MagazinePreviewProps = {
   magazine: Magazine;
 };
 
-export const MagazinePreview = ({ magazine }: MagazinePreviewProps) => (
-  <Grid container spacing={2} justifyContent="center" gap="20px" position="sticky" top="215px">
-    <Grid
-      container
-      spacing={2}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      gap="40px"
-    >
-      <Grid>
-        <Chip
-          icon={<FavoriteBorderIcon />}
-          clickable
-          sx={{
-            width: '54px',
-            height: '40px',
-            paddingLeft: '10px'
-          }}
-        />
-      </Grid>
-      <Grid>
-        <Chip
-          icon={<MenuBookIcon />}
-          clickable
-          sx={{
-            width: '54px',
-            height: '40px',
-            paddingLeft: '10px'
-          }}
-        />
-      </Grid>
-    </Grid>
+export const MagazinePreview = ({ magazine }: MagazinePreviewProps) => {
+  const { enqueueSnackbar } = useSnackbar();
 
-    <Grid display="flex" justifyContent="center" alignItems="center" height="400px">
-      <img
-        // TODO: Replcae with url when blob storage is ready
-        // eslint-disable-next-line
-        src={require(`../../../assets/images/${magazine.image}`)}
-        alt={magazine.name}
-        style={{ borderRadius: '10px', objectFit: 'contain', maxWidth: '100%', height: '100%  ' }}
-      />
+  const handleFavorite = () => {
+    enqueueSnackbar('Functionality is not implemented yet', { variant: 'error' });
+  };
+
+  const handleReadDemo = () => {
+    enqueueSnackbar('Functionality is not implemented yet', { variant: 'error' });
+  };
+  return (
+    <Grid container spacing={2} justifyContent="center" gap="20px" position="sticky" top="215px">
+      <Grid
+        container
+        spacing={2}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        gap="40px"
+      >
+        <Grid>
+          <Chip
+            icon={<FavoriteBorderIcon />}
+            clickable
+            onClick={handleFavorite}
+            sx={{
+              width: '54px',
+              height: '40px',
+              paddingLeft: '10px'
+            }}
+          />
+        </Grid>
+        <Grid>
+          <Chip
+            icon={<MenuBookIcon />}
+            clickable
+            onClick={handleReadDemo}
+            sx={{
+              width: '54px',
+              height: '40px',
+              paddingLeft: '10px'
+            }}
+          />
+        </Grid>
+      </Grid>
+
+      <Grid display="flex" justifyContent="center" alignItems="center" height="400px">
+        <img
+          // TODO: Replcae with url when blob storage is ready
+          // eslint-disable-next-line
+          src={require(`../../../assets/images/${magazine.image}`)}
+          alt={magazine.name}
+          style={{ borderRadius: '10px', objectFit: 'contain', maxWidth: '100%', height: '100%  ' }}
+        />
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
